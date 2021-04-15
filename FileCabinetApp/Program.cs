ca7976c090ed52f3a2b91b41192be34cf97411b5
Anh Tuan Phan Tran <fanchana@yandex.ru>
2021-04-15 15:22:24 +0300

    Add initial version of FileCabinetApp.

--- FileCabinetApp/Program.cs
+++ FileCabinetApp/Program.cs
@@ -2,9 +2,9 @@
 
 namespace FileCabinetApp
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
             Console.WriteLine("Hello World!");
         }
