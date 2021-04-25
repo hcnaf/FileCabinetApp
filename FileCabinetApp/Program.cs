f45f0c59ae302b7fda447cc1de32ebe1a48e73c3
Anh Tuan Phan Tran <fanchana@yandex.ru>
2021-04-25 17:51:47 +0300

    Refactor user input validation.

--- FileCabinetApp/Program.cs
+++ FileCabinetApp/Program.cs
@@ -40,6 +40,7 @@ namespace FileCabinetApp
         };
 
         private static IFileCabinetService fileCabinetService;
+        private static IDataInput dataInput;
 
         private static bool isRunning = true;
 
@@ -54,6 +55,7 @@ namespace FileCabinetApp
             if (args.Length == 0)
             {
                 fileCabinetService = new FileCabinetService(new DefaultValidator());
+                dataInput = new DefaultDataInput();
                 Console.WriteLine("Using default validation rules.");
             }
             else if (args.Length == 1)
@@ -103,17 +105,20 @@ namespace FileCabinetApp
                 if (args[1].Equals("custom", StringComparison.InvariantCultureIgnoreCase))
                 {
                     fileCabinetService = new FileCabinetService(new CustomValidator());
+                    dataInput = new CustomDataInput();
                     Console.WriteLine("Using custom validation rules.");
                 }
                 else
                 {
                     fileCabinetService = new FileCabinetService(new DefaultValidator());
+                    dataInput = new DefaultDataInput();
                     Console.WriteLine("Using default validation rules.");
                 }
             }
             else
             {
                 fileCabinetService = new FileCabinetService(new DefaultValidator());
+                dataInput = new DefaultDataInput();
                 Console.WriteLine("Using default validation rules.");
             }
         }
@@ -159,7 +164,7 @@ namespace FileCabinetApp
 
         private static void Create(string parameters)
         {
-            Console.WriteLine($"Record #{fileCabinetService.CreateRecord(DataInput())} is created");
+            Console.WriteLine($"Record #{fileCabinetService.CreateRecord(dataInput.DataInput())} is created");
         }
 
         private static void Edit(string parameters)
@@ -172,7 +177,7 @@ namespace FileCabinetApp
                 return;
             }
 
-            fileCabinetService.EditRecord(id, DataInput());
+            fileCabinetService.EditRecord(id, dataInput.DataInput());
             Console.WriteLine($"Record #{id} was edited.");
         }
 
@@ -221,269 +226,6 @@ namespace FileCabinetApp
         {
             Console.WriteLine("Exiting an application...");
             isRunning = false;
-        }
-
-        private static FileCabinetRecord DataInput()
-        {
-            var record = new FileCabinetRecord();
-
-            Console.Write("First name: ");
-            record.FirstName = ReadInput(
-                new Func<string, Tuple<bool, string, string>>(StringConverter),
-                new Func<string, Tuple<bool, string>>(FirstNameValidator));
-
-            Console.Write("Last name: ");
-            record.LastName = ReadInput(
-                new Func<string, Tuple<bool, string, string>>(StringConverter),
-                new Func<string, Tuple<bool, string>>(LastNameValidator));
-
-            Console.Write("Date of birth: ");
-            record.DateOfBirth = ReadInput(
-                new Func<string, Tuple<bool, string, DateTime>>(DateConverter),
-                new Func<DateTime, Tuple<bool, string>>(DateOfBirthValidator));
-
-            Console.Write("Balance: ");
-            record.Balance = ReadInput(
-                new Func<string, Tuple<bool, string, decimal>>(DecimalConverter),
-                new Func<decimal, Tuple<bool, string>>(BalanceValidator));
-
-            Console.Write("Secure charecter: ");
-            record.SecurityCharacter = ReadInput(
-                new Func<string, Tuple<bool, string, char>>(CharConverter),
-                new Func<char, Tuple<bool, string>>(SecurityCharecterValidator));
-
-            Console.Write("Secure number: ");
-            record.SecurityNumber = ReadInput(
-                new Func<string, Tuple<bool, string, short>>(ShortConverter),
-                new Func<short, Tuple<bool, string>>(SecurityNumberValidator));
-
-            Console.Write("Payment system (Visa/MasterCard): ");
-            record.PaymentSystem = ReadInput(
-                new Func<string, Tuple<bool, string, string>>(StringConverter),
-                new Func<string, Tuple<bool, string>>(PaymentSystemValidator));
-
-            Console.Write("Residence (True/False): ");
-            record.Residency = ReadInput(
-                new Func<string, Tuple<bool, string, bool>>(BooleanConverter),
-                new Func<bool, Tuple<bool, string>>(ResidenceValidator));
-
-            Console.Write("Country code: ");
-            record.CountryCode = ReadInput(
-                new Func<string, Tuple<bool, string, int>>(IntConverter),
-                new Func<int, Tuple<bool, string>>(CountryCodeValidator));
-
-            return record;
-        }
-
-        private static T ReadInput<T>(Func<string, Tuple<bool, string, T>> converter, Func<T, Tuple<bool, string>> validator)
-        {
-            do
-            {
-                T value;
-
-                var input = Console.ReadLine();
-                var conversionResult = converter(input);
-
-                if (!conversionResult.Item1)
-                {
-                    Console.WriteLine($"Conversion failed: {conversionResult.Item2}. Please, correct your input.");
-                    continue;
-                }
-
-                value = conversionResult.Item3;
-
-                var validationResult = validator(value);
-                if (!validationResult.Item1)
-                {
-                    Console.WriteLine($"Validation failed: {validationResult.Item2}. Please, correct your input.");
-                    continue;
-                }
-
-                return value;
-            }
-            while (true);
-        }
-
-        private static Tuple<bool, string, string> StringConverter(string str)
-        {
-            if (str is null)
-            {
-                return new Tuple<bool, string, string>(false, "Value is null", string.Empty);
-            }
-
-            return new Tuple<bool, string, string>(true, string.Empty, str);
-        }
-
-        private static Tuple<bool, string> FirstNameValidator(string firstName)
-        {
-            if (firstName.Length < 2 || firstName.Length > 60 || firstName.Contains(' '))
-            {
-                return new Tuple<bool, string>(false, "Invalid first name");
-            }
-
-            return new Tuple<bool, string>(true, string.Empty);
-        }
-
-        private static Tuple<bool, string> LastNameValidator(string lastName)
-        {
-            if (lastName.Length < 2 || lastName.Length > 60 || lastName.Contains(' '))
-            {
-                return new Tuple<bool, string>(false, "Invalid last name.");
-            }
-
-            return new Tuple<bool, string>(true, string.Empty);
-        }
-
-        private static Tuple<bool, string> PaymentSystemValidator(string paymentSystem)
-        {
-            if (!paymentSystem.Equals("Visa", StringComparison.InvariantCultureIgnoreCase) &&
-                !paymentSystem.Equals("MasterCard", StringComparison.InvariantCultureIgnoreCase))
-            {
-                return new Tuple<bool, string>(false, "Invalid payment system.");
-            }
-
-            return new Tuple<bool, string>(true, string.Empty);
-        }
-
-        private static Tuple<bool, string, DateTime> DateConverter(string date)
-        {
-            string[] dayMonthYear = date.Split('/');
-            if (!int.TryParse(dayMonthYear[0], out int day))
-            {
-                return new Tuple<bool, string, DateTime>(
-                    false,
-                    "Invalid day.",
-                    DateTime.MinValue);
-            }
-
-            if (!int.TryParse(dayMonthYear[1], out int month))
-            {
-                return new Tuple<bool, string, DateTime>(
-                    false,
-                    "Invalid month.",
-                    DateTime.MinValue);
-            }
-
-            if (!int.TryParse(dayMonthYear[2], out int year))
-            {
-                return new Tuple<bool, string, DateTime>(
-                    false,
-                    "Invalid year.",
-                    DateTime.MinValue);
-            }
-
-            return new Tuple<bool, string, DateTime>(
-                true,
-                string.Empty,
-                new DateTime(year, month, day));
-        }
-
-        private static Tuple<bool, string> DateOfBirthValidator(DateTime dateOfBirth)
-        {
-            if (dateOfBirth > DateTime.Now || dateOfBirth < new DateTime(1950, 1, 1))
-            {
-                return new Tuple<bool, string>(false, "Date of birth is out of range.");
-            }
-
-            return new Tuple<bool, string>(true, string.Empty);
-        }
-
-        private static Tuple<bool, string, char> CharConverter(string ch)
-        {
-            if (ch.Length < 1)
-            {
-                return new Tuple<bool, string, char>(false, "Security character is empty.", '\0');
-            }
-
-            return new Tuple<bool, string, char>(true, string.Empty, ch[0]);
-        }
-
-        private static Tuple<bool, string> SecurityCharecterValidator(char securityCharecter)
-        {
-            if (!char.IsLetter(securityCharecter))
-            {
-                return new Tuple<bool, string>(false, "Securuty charecter must be a letter.");
-            }
-
-            return new Tuple<bool, string>(true, string.Empty);
-        }
-
-        private static Tuple<bool, string, int> IntConverter(string number)
-        {
-            if (int.TryParse(number, out int res))
-            {
-                return new Tuple<bool, string, int>(true, string.Empty, res);
-            }
-
-            return new Tuple<bool, string, int>(false, "Invalid number.", -1);
-        }
-
-        private static Tuple<bool, string> CountryCodeValidator(int number)
-        {
-            if (number < 1 || number > 999)
-            {
-                return new Tuple<bool, string>(false, "Invalid country code.");
-            }
-
-            return new Tuple<bool, string>(true, string.Empty);
-        }
-
-        private static Tuple<bool, string, short> ShortConverter(string number)
-        {
-            if (short.TryParse(number, out short res))
-            {
-                return new Tuple<bool, string, short>(true, string.Empty, res);
-            }
-
-            return new Tuple<bool, string, short>(false, "Invalid number.", -1);
-        }
-
-        private static Tuple<bool, string> SecurityNumberValidator(short number)
-        {
-            if (number < 0 || number > 9999)
-            {
-                return new Tuple<bool, string>(false, "Invalid security number.");
-            }
-
-            return new Tuple<bool, string>(true, string.Empty);
-        }
-
-        private static Tuple<bool, string, decimal> DecimalConverter(string dec)
-        {
-            if (decimal.TryParse(dec, out decimal res))
-            {
-                return new Tuple<bool, string, decimal>(true, string.Empty, res);
-            }
-            else
-            {
-                return new Tuple<bool, string, decimal>(false, "Invalid decimal value.", decimal.MinValue);
-            }
-        }
-
-        private static Tuple<bool, string> BalanceValidator(decimal balance)
-        {
-            return new Tuple<bool, string>(true, string.Empty);
-        }
-
-        private static Tuple<bool, string, bool> BooleanConverter(string b)
-        {
-            if (b.Equals("true", StringComparison.InvariantCultureIgnoreCase))
-            {
-                return new Tuple<bool, string, bool>(true, string.Empty, true);
-            }
-            else if (b.Equals("false", StringComparison.InvariantCultureIgnoreCase))
-            {
-                return new Tuple<bool, string, bool>(true, string.Empty, false);
-            }
-            else
-            {
-                return new Tuple<bool, string, bool>(false, "Invalid residence", false);
-            }
-        }
-
-        private static Tuple<bool, string> ResidenceValidator(bool residence)
-        {
-            return new Tuple<bool, string>(true, string.Empty);
         }
     }
 }
